0a90ac7d05a323b564edbae528ed42dfd97eca58
Oulewappeur <antonmol.90@gmail.com>
2026-01-26 21:01:21 +0100

    Fix: base path toegevoegd voor GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Voeg de onderstaande regel toe. 
+  // Dit zorgt ervoor dat alle verwijzingen naar scripts en styles kloppen op GitHub Pages.
+  base: '/3d-print-hub/', 
 })
